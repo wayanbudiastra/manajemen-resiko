77f2298fc96d9e12865657515591a1ec36c5ed43
wayan budiastra <wayanbudiastra@wayans-MacBook-Air.local>
2025-06-30 10:54:13 +0800

    update monitoring dashboard fix

--- app/Helpers/Global.php
+++ app/Helpers/Global.php
@@ -242,7 +242,7 @@ return $data;
 
 function rekap_risk_unit($unit,$tahun,$bulan,$grade){
 $total = 0;
-$data = Risk_register_pelaporan::where('unit_id',$unit)->where('periode_laporan',$tahun.''.$bulan)->where('matrik_kontrol_grade',$grade)->where('posting','Y')->count();
+$data = Risk_register_pelaporan::where('unit_id',$unit)->where('periode_laporan',$tahun.''.$bulan)->where('matrik_monitoring_grade',$grade)->where('posting','Y')->count();
 if($data > 0){
     $total = $data;
 }
